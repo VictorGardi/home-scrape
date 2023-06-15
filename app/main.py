3cd1a7f38b2eeed56f819de1899fc5711fc43538
Victor <victor.olssn@gmail.com>
2023-06-15 20:52:52 +0200

    try with st pandas profiling

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import pandas_profiling
 import streamlit as st
+from sklearn.linear_model import LinearRegression
+from streamlit_pandas_profiling import st_profile_report
 
 from home_scrape.sql import get_engine, get_values
 
@@ -8,38 +11,77 @@ TABLE = "houses"
 MODELS = ["linear regression", "Ridge regression"]
 
 
-@st.cache_data
+# @st.cache_data
 def fetch_data() -> pd.DataFrame:
     vals = get_values(get_engine(), table=TABLE, schema=SCHEMA)
     df = pd.DataFrame(vals)
     df.drop(columns=["index"], inplace=True)
+    df["antal rum"] = df["antal rum"].str.replace(",", ".")
+    df["pris"] = df["pris"].str.replace(" ", "")
     return df
 
 
 def main():
+    st.header("Create your dataset")
     orig_df = fetch_data()
-    target_col, feature_col = st.columns(2)
-    target = target_col.selectbox("Choose a target variable", orig_df.columns.to_list())
-    cols = feature_col.multiselect(
-        "Choose features to use in an 'X' vector", orig_df.columns.to_list()
-    )
-    df = orig_df[cols + [target]]
-    st.write(f"Number of data points: {len(orig_df)}")
-    st.dataframe(df)
-
-    if st.button("Clean data"):
+    with st.expander("Choose columns of interest"):
+        target_col, feature_col = st.columns(2)
+        target_col.text(f"Number of rows in data: {len(orig_df)}")
+        feature_col.text(f"Number of columns in data: {len(orig_df.columns)}")
+        target = target_col.selectbox(
+            "Choose a target variable", orig_df.columns.to_list()
+        )
+        feature_cols = feature_col.multiselect(
+            "Choose features to use in an 'X' vector", orig_df.columns.to_list()
+        )
+        df = orig_df[feature_cols + [target]]
         df.dropna(inplace=True)
-        st.success(f"Cleaned dataframe. Current number of datapoints: {len(df)}")
+        st.dataframe(df.head(10))
+    with st.expander("Choose data type for each column"):
+        dtypes = ["numeric", "str"]
+        orig_column_types = {k: [None] for k in df.columns}
+        t = {
+            k: st.column_config.SelectboxColumn(
+                options=dtypes,
+                width="medium",
+                help="Choose the preferred data type",
+                required=True,
+            )
+            for k in orig_column_types.keys()
+        }
+        column_types = st.data_editor(orig_column_types, column_config=t)
+        convert = st.button("Convert data..")
+        if convert:
+            for k, v in column_types.items():
+                if "str" in v:
+                    df[k] = df[k].astype("string")
+                elif "numeric" in v:
+                    df[k] = pd.to_numeric(df[k], "coerce")
 
-    col1, col2 = st.columns(2)
-    models = col1.multiselect("Choose models to test", MODELS)
+            st.success("Completed conversion successfully!")
+            st.write(df.dtypes)
+    with st.expander("Get insights about the data"):
+        pass
+        # pr = df.profile_report()
+        # st_profile_report(pr)
+
+    # for x in feature_cols:
+    #    ax = df.plot(kind="scatter", x=x, y=target)
+    #    st.pyplot(ax.figure)
+    # st.dataframe(df)
+
+    X = df[feature_cols]
+    y = df[target]
+
+    st.header("Train models")
+    models = st.multiselect("Choose models to use", MODELS)
     if models:
-        tabs = col2.tabs(models)
+        tabs = st.tabs(models)
 
-    for tab in tabs:
-        print(tab)
+        for tab in tabs:
+            tab.write("hej")
 
 
 if __name__ == "__main__":
-    st.set_page_config(layout="wide")
+    # st.set_page_config(layout="wide")
     main()
